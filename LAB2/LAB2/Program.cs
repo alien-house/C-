13f8c0375e4919421a537b22c3ab4ea6e6ab0cd7
alien-house <info@alien-house.com>
2017-09-21 17:16:04 -0700

    add lab2

---
+++ LAB2/LAB2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LAB2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int num = 5;
+			for (int i = 0; i < num; i++)
+			{
+                for (int a = 0; a < i; a++)
+				{
+					Console.Write('*');
+					//Console.Write(String.Format("{0, 10}", i));
+				}
+				Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ LAB2/Task2_Matrices/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task2_Matrices
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Input the size of the square matrix (less than 5):");
+			int size = int.Parse(Console.ReadLine());
+            int squreSize = 2;
+			Console.WriteLine("------------");
+            int[] sizes = new int[size];
+
+            int[][][] box = new int[size][][];
+            for (int index = 0; index < size; index++)
+            {
+                box[index] = new int[3][];
+            }
+
+            for (int index = 0; index < size; index++)
+			{
+				Console.WriteLine("[{0}]=============== ", index);
+                for (int i = 0; i < squreSize; i++)
+                {
+                    Console.WriteLine("[{0}] ", i);
+                    box[index][i] = new int[2];
+                    for (int a = 0; a < squreSize; a++)
+                    {
+						box[index][i][a] = int.Parse(Console.ReadLine());
+                        Console.Write("{0}{1} :", i, a);
+                        Console.WriteLine("{0} ", box[index][i][a]);
+                    }
+                }
+            }
+
+
+        }
+    }
+}
